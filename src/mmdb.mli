6a5991e447ec679fc5f11d9ff632c863d4aef470
Martin Slota <msl@issuu.com>
2019-03-20 09:23:08 +0100

    A suggested interface for making custom queries through libmaxminddb

--- src/mmdb.mli
+++ src/mmdb.mli
@@ -34,21 +34,89 @@ module Lookup_ip_error : sig
   [@@deriving show]
 end
 
-module Lookup_error : sig
-  type t = [`Unsupported_data_type of string | Lookup_ip_error.t]
-  [@@deriving show]
+module Fetch_value_error : sig
+  (* `Unexpected_data_type is new - I think `Unsupported_data_type is abused
+     in one spot so I split it *)
+  type t =
+    [ `Unsupported_data_type of string
+    | `Unexpected_data_type of string
+    | Common_error.t ]
 end
+
+module Lookup_error : sig
+  type t = [Fetch_value_error.t | Lookup_ip_error.t] [@@deriving show]
+end
+
+module Path = Types.Path
+
+(** Open an MMDB file and return a reference to it *)
+val open_file : Path.t -> (t, Open_file_error.t) result
+
+module Ip = Types.Ip
+
+module Ip_data : sig
+  type t
+end
+
+val look_up_ip : t -> Ip.t -> (Ip_data.t, Lookup_ip_error.t) result
 
 module Lookup_result : sig
   type 'a t = ('a option, Lookup_error.t) result
 end
 
-module Path = Types.Path
-module Ip = Types.Ip
-module Coordinates = Coordinates
+module Fetch_result : sig
+  type 'a t = ('a option, Fetch_value_error.t) result
+end
 
-(** Open an MMDB file and return a reference to it *)
-val open_file : Path.t -> (t, Open_file_error.t) result
+module type VALUE_TYPE = sig
+  module Query : sig
+    type t
+
+    val of_string_list : string list -> t
+
+    val to_string_list : t -> string list
+  end
+
+  module Answer : sig
+    type t
+  end
+
+  val look_up : t -> Ip.t -> Query.t -> Answer.t Lookup_result.t
+
+  val fetch : Ip_data.t -> Query.t -> Answer.t Fetch_result.t
+end
+
+type any_answer =
+  | String of string
+  | Float of float
+  | Int of int
+  | Bool of bool
+
+module Any : VALUE_TYPE with type Answer.t = any_answer
+
+module String : sig
+  include VALUE_TYPE with type Answer.t = string
+
+  (* usage: sth. like `Mmdb.String.(look_up mmdb ip country_code)` *)
+  (* usage: sth. like `Mmdb.String.(fetch ip_data country_code)` *)
+  val country_code : Query.t
+
+  val region_code : Query.t
+end
+
+module Coordinates_ : sig
+  include VALUE_TYPE with type Answer.t = Coordinates.t
+
+  val coordinates : Query.t
+end
+
+module Float : VALUE_TYPE with type Answer.t = float
+
+module Int : VALUE_TYPE with type Answer.t = int
+
+module Bool : VALUE_TYPE with type Answer.t = bool
+
+module Coordinates = Coordinates
 
 (** Determine the coordinates of an IP *)
 val coordinates : t -> Ip.t -> Coordinates.t Lookup_result.t
